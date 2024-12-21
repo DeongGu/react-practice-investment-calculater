c4cd848ea06890609a04c5d89e051ced2735c344
SeongWoo Shin <ssw4510@gmail.com>
2024-12-21 23:18:46 +0900

    add: Header style css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
+import "./Header.css";
+
 export default function Header() {
   return (
     <>
-      <div>
+      <div className="header">
         <img src="logo.png" alt="로고이미지" width={100} />
         <p>Investment Calculator</p>
       </div>
